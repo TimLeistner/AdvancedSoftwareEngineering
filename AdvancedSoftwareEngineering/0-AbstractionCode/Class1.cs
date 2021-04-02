dc4620dcb96827166aad3f1acf8d978226d8b99a
Tim Leistner <tim.leistner99@gmail.com>
2021-04-02 10:48:37 +0200

    Groundwork / Basic Concept
    Add general structure and first steps for UI.

---
+++ AdvancedSoftwareEngineering/0-AbstractionCode/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace _0_AbstractionCode
+{
+    public class Class1
+    {
+    }
+}
---
+++ AdvancedSoftwareEngineering/1-DomainCode/ValueObjects/Money.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using _1_Domain_Code.Enums;
+
+namespace _1_Domain_Code.ValueObjects
+{
+    public sealed class Money
+    {
+        private double value;
+        private Currency currency;
+
+        public Money(double value, Currency currency)
+        {
+            this.value = value;
+            this.currency = currency;
+        }
+
+        public double GetValue()
+        {
+            double result = value;
+            return result;
+        }
+
+        public Currency GetCurrency()
+        {
+            Currency result = currency;
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Money money &&
+                   value == money.value &&
+                   currency == money.currency;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 2139521936;
+            hashCode = hashCode * -1521134295 + value.GetHashCode();
+            hashCode = hashCode * -1521134295 + currency.GetHashCode();
+            return hashCode;
+        }
+    }
+}
