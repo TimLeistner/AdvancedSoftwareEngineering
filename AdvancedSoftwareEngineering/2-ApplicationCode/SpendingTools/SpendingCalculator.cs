24f0ef6d52ea12ef9d1b5c6ce1654493843483be
Tim Leistner <tim.leistner99@gmail.com>
2021-04-06 11:04:59 +0200

    Add SpendingTools
    Add spending tools in ApplicationCode. Implement functionality for SpendingCalculator.
    Adjust resolution for UI

---
+++ AdvancedSoftwareEngineering/2-ApplicationCode/SpendingTools/SpendingCalculator.cs
@@ -0,0 +1,41 @@
+﻿using _1_Domain_Code.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_ApplicationCode.SpendingTools
+{
+    public class SpendingCalculator
+    {
+        Category category;
+        SpendingCalculator(Category category)
+        {
+            this.category = category;
+        }
+
+        public bool IsLimitExceeded(DateTime month)
+        {
+            bool result = false;
+            List<Spending> spendingOfMonth = SpendingSorter.GetSpendingByMonth(category.GetSpendings(), month);
+
+            double limit = category.GetLimit().GetValue();
+            double sumOfSpending = GetSumOfSpending(spendingOfMonth);
+
+            result = limit < sumOfSpending;
+
+            return result;
+        }
+
+        public double GetSumOfSpending(List<Spending> spendings)
+        {
+            double sumOfSpending = 0;
+
+            spendings.ForEach((spending) =>
+            {
+                sumOfSpending += spending.GetSpendMoney().GetValue();
+            });
+
+            return sumOfSpending;
+        }
+    }
+}
---
+++ AdvancedSoftwareEngineering/2-ApplicationCode/SpendingTools/SpendingSorter.cs
@@ -0,0 +1,17 @@
+﻿using _1_Domain_Code.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_ApplicationCode.SpendingTools
+{
+    public class SpendingSorter
+    {
+        SpendingSorter() { }
+
+        public static List<Spending> GetSpendingByMonth(List<Spending> spendings, DateTime month)
+        {
+            return null;
+        }
+    }
+}
